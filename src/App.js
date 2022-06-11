b0650b4fa133213052a31c7789677bc26a5887da
farmology <abkim03@gmail.com>
2022-06-10 23:21:20 -0700

    Add basic form and hook for name input
    Add basic form structure in App.js, use hook to display name input on
    page in real time.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [test, setTest] = useState();
+
+  function logConsole(e) {
+    e.preventDefault();
+    console.log(test);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={logConsole}>
+        <label htmlFor='name'>Name</label>
+        <input onChange={(e) => {setTest(e.target.value)}} type="text" id="name"/>
+        <label htmlFor='email'>E-mail</label>
+        <input type="text" id="email"/>
+        <label htmlFor='number'>Phone Number</label>
+        <input type="text" id="number"/>
+        <label htmlFor='schoolname'>School</label>
+        <input type="text" id="schoolname"/>
+        <label htmlFor='schoolyear'>Years Attended</label>
+        <input type="text" id="schoolyear"/>
+        <button type="submit">Submit</button>
+      </form>
+      <h1>{test}</h1>
+
     </div>
   );
 }
